50e0ce8870490cb93052e998f463050c6f9a608d
FR <naoki.maekawa.511@gmail.com>
2023-12-06 21:55:19 +0900

    12/6 496 ver2

--- BcryptDemo/index.js
+++ BcryptDemo/index.js
@@ -1,24 +1,53 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 // const hashPassword = async (pw) => {
-//     const salt = await bcrypt.genSalt(12);
+//     const salt = await bcrypt.genSalt(10);
 //     const hash = await bcrypt.hash(pw, salt);
 //     console.log(salt);
 //     console.log(hash);
 // }
+
 const hashPassword = async (pw) => {
     const hash = await bcrypt.hash(pw, 12);
     console.log(hash);
 }
 
+
 const login = async (pw, hashedPw) => {
-    const result = await bcrypt.compare(pw, hashedPw);
-    if (result) {
-        console.log('ログイン成功！！！！');
-    } else {
-        console.log('ログイン失敗！！！');
-    }
+  const result = await bcrypt.compare(pw, hashedPw);
+  if (result) {
+    console.log(`ログイン成功！！！`);
+  } else {
+    console.log(`ログイン失敗！！！`);
+  }
 }
 
-// hashPassword('123456');
-login('123457', '$2b$12$lo.ahyWPPqhLd4RoK560..wLprbxgz0bM4L0iN8SyC37nps/zFXNW');
+login(`123456`, `$2b$10$CDuqGc51jZbCJVOxNAJiBek8HjPVse8SKirhbtnMr.Lc38x8O1XGq`);
+
+// hashPassword(`123456`);
+
+
+// const bcrypt = require('bcrypt');
+
+// // const hashPassword = async (pw) => {
+// //     const salt = await bcrypt.genSalt(12);
+// //     const hash = await bcrypt.hash(pw, salt);
+// //     console.log(salt);
+// //     console.log(hash);
+// // }
+// const hashPassword = async (pw) => {
+//     const hash = await bcrypt.hash(pw, 12);
+//     console.log(hash);
+// }
+
+// const login = async (pw, hashedPw) => {
+//     const result = await bcrypt.compare(pw, hashedPw);
+//     if (result) {
+//         console.log('ログイン成功！！！！');
+//     } else {
+//         console.log('ログイン失敗！！！');
+//     }
+// }
+
+// // hashPassword('123456');
+// login('123457', '$2b$12$lo.ahyWPPqhLd4RoK560..wLprbxgz0bM4L0iN8SyC37nps/zFXNW');
